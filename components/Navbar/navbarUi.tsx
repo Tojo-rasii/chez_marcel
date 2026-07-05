af087c766ee8f4a65a36709755cdc6ef6dcb6914
Tojo rasii <tojorasii1619@gmail.com>
2026-07-06 00:23:07 +0300

    test

--- components/Navbar/navbarUi.tsx
+++ components/Navbar/navbarUi.tsx
@@ -4,7 +4,7 @@ import NavLinks from "./navLinks";
 export default function NavbarUi() {
     return (
         <div className="absolute w-full top-0">
-            <div className="flex items-center justify-between h-16 p-5 bg-black text-white">
+            <div className="flex items-center justify-between h-16 p-5 text-current">
                 <Logo />
 
                 <div className="flex items-center justify-end">
