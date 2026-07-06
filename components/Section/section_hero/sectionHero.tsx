02dfc4da6ee8e9aeae5664801dfd25b59deaad53
Tojo rasii <tojorasii1619@gmail.com>
2026-07-06 13:23:51 +0300

    fix:error

--- components/Section/section_hero/sectionHero.tsx
+++ components/Section/section_hero/sectionHero.tsx
@@ -54,7 +54,7 @@ export default function SectionHero({ activePage }: any) {
 
         {activePage !== 3 && activePage !== 4 && activePage !== 5 && activePage !== 6 && (
           <div className="h-full m-auto bg-red-200 w-full flex items-center justify-center">
-            <HeroServiceContenu page={activePage} />
+            <HeroServiceContenu page={activePage} activePage={activePage} />
           </div>
         )}
 
